c36b32e32247c8ef35ced4ae0a8ec89af144540c
sn49 <34567701+sn49@users.noreply.github.com>
2021-05-21 09:19:35 +0900

    Update chatFilter.py

--- chatFilter.py
+++ chatFilter.py
@@ -177,10 +177,7 @@ async def 제한음챗(ctx):
             name=f"5분 음챗(남은 시간 : {limit}초)", category=cate
         )
 
-        for i in range(30):
-            await asyncio.sleep(10)
-            limit -= 10
-            await chan.edit(name=f"5분 음챗(남은 시간 : {limit}초)")
+        await asyncio.sleep(300)
 
         await chan.delete()
         tempvoice = False
